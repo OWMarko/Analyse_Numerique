43b8a781f8af0eb7b1e06b1c26dcdc8f9ed1e80e
Marko <sinadinovic.marko.professionnel@gmail.com>
2025-04-10 17:19:41 +0200

    Update Rectangles_a_droite.py

--- Codes/Rectangles_a_droite.py
+++ Codes/Rectangles_a_droite.py
@@ -22,3 +22,18 @@ def quadNumRD(f,a,b,M):
 def f(x):
     return x**2
 
+# Cas élémentaire
+a, b = 0, 1
+result_elementaire = quadNumRD(f,a,b)
+print(result_elementaire)
+
+# Cas uniforme
+M = 10
+result_uniforme = quadNumRD(f,a,b,M)
+print(result_uniforme)
+
+# Cas non uniforme
+x_points = [0, 0.25, 0.5, 0.75, 1.0]  
+result_non_uniforme = quadNumRD(f,a,b,M)
+print(result_non_uniforme)
+
