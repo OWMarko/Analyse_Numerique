96671ebb0fb4245f296909a3de73f6cb71a30fc4
Marko <sinadinovic.marko.professionnel@gmail.com>
2025-04-10 17:12:08 +0200

    Update Rectangles_a_gauche.py

--- Codes/Rectangles_a_gauche.py
+++ Codes/Rectangles_a_gauche.py
@@ -1,7 +1,22 @@
 import numpy as np
 from matplotlib.pyplot import * 
 
+#élémentaire
 def quadNumRG(f,a,b,M):
-    x = linspace(a,b,M)
+    x = np.linspace(a,b,M)
     h = x[1] - x[0]
     return sum(f(x[:-1]))*h
+
+
+#composé non uniforme
+def quadNumRG_Compose(f,x):
+    h = np.diff(x)
+    return np.sum(f(x[:-1]) * h)
+
+#composé uniforme
+def quadNumRG_Compose_Uniforme(f,x):
+    x = np.linspace(a,b,M)
+    h = x[1] - x[0]
+    return np.sum(f(x[:-1]) * h
+
+    
