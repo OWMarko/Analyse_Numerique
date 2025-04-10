00f680afcc7cd3cb6e7fbd7c2675d633ed4b613c
Marko <sinadinovic.marko.professionnel@gmail.com>
2025-04-10 17:18:44 +0200

    Update Rectangles_a_droite.py

--- Codes/Rectangles_a_droite.py
+++ Codes/Rectangles_a_droite.py
@@ -1,1 +1,24 @@
+#élémentaire
+
+def quadNumRD(f,a,b):
+  return (b-a)*f(a)
+
+
+#Composé non uniforme
+def quadNumRD(f,a,b,M):
+    h = np.diff(x)
+    return sum(f(x[1:])*)h
+
+
+#Composé uniforme
+def quadNumRD(f,a,b,M):
+    x = linspace(a,b,M)
+    h = x[1] - x[0]
+    return sum(f(x[1:]))*h
+
+
+#Utilisation : 
+
+def f(x):
+    return x**2
 
