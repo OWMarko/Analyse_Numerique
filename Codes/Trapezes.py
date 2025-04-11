6d78760cbb52a05e7bd7214b51c37f42d1001913
Marko <sinadinovic.marko.professionnel@gmail.com>
2025-04-11 22:45:12 +0200

    Update Trapezes.py

--- Codes/Trapezes.py
+++ Codes/Trapezes.py
@@ -6,7 +6,7 @@ def quadraNumTT(f,a,b):
 
 #Composé non uniforme
 def quadraNumTT_nnuni(f,x):
-  h = np.diff(x) #x[1] - x[0]
+  h = np.diff(x) #
   return sum(h * (f([x1:]+f(x[:-1]) /2 #sinon tu peux aussi mettre x[1:] - x[:-1] à la place du h
                     
 #Composé uniforme
