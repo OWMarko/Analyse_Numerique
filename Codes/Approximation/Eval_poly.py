d7a0f1d14151609c8704c2e575bfd4d97d46fd13
Marko <sinadinovic.marko.professionnel@gmail.com>
2025-04-22 16:00:02 +0200

    Update Eval_poly.py

--- Codes/Approximation/Eval_poly.py
+++ Codes/Approximation/Eval_poly.py
@@ -14,3 +14,15 @@ def eval_poly_vect(a,z) :
          for j in range(N) :
             P[j]=valpoly(a,z[j])
          return P
+    
+#Si on cherch les coefs du poly p
+def coef(x,y): 
+    x = np.array(x)
+    y = np.array(y)
+    n = len(x) - 1
+    a = np.zeros(n)
+    d = np.zeros(n)
+    for i in range(n):
+        a[i] = (y[i+1] - y[i]) / (x[i+1] - x[i])
+        d[i] = y[i] - a[i]*x[i]
+    return a, d
