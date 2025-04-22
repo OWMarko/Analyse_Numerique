3c3301d5f8d2d2d47ef3fdec8883d0137c635e78
Marko <sinadinovic.marko.professionnel@gmail.com>
2025-04-22 15:41:48 +0200

    Update Diff_divisee.py

--- Codes/Approximation/Diff_divisee.py
+++ Codes/Approximation/Diff_divisee.py
@@ -1,1 +1,12 @@
+import numpy as np
+
+def diffdiv(x, y):
+    n =  len(x)
+    F = np.zeros((n, n))
+    F[:, 0] = y  
+    for j in range(1, n):
+        for i in range(n - j):
+            F[i, j] = (F[i+ 1,j - 1] - F[i,j - 1]) / (x[i+j] - x[i])
+
+    return F[0, :]
 
