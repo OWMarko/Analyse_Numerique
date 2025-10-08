877d7845f98414fed012e356d49e3893d4a46606
Marko <sinadinovic.marko.professionnel@gmail.com>
2025-10-08 12:57:14 +0200

    Import Cheat_sheet_python.py

---
+++ Codes/Cheat_sheet_python.py
@@ -0,0 +1,49 @@
+#Dictionnaire
+depart = {
+    "Var": 83,
+    "Isere": 38,
+    "Drome": 26,
+    "Vaucluse": 86,
+    "Finistere": 29,
+    "Cher": 18
+}
+
+#Extraire les valeurs selon une condition valeur < 30
+
+valeurs_inf_30 = [valeur for cle, valeur in depart.items() if valeur < 30]
+
+#Trier notre liste
+
+valeurs_inf_30_triees = sorted(valeurs_inf_30)
+
+#Extraire les valeurs selon une condition clefs avec un nombre de lettre
+
+cles_6_lettres = [cle for cle in depart if len(cle) >= 6]
+
+#Trier notre liste selon leur valeur ordre croissant (reverse = False)
+
+cles_6_lettres_triees = sorted(cles_6_lettres, key=lambda x: depart[x], reverse = False)
+
+#Trier notre liste selon l'ordre alphab. 
+
+cles_6_filtrees_triees = sorted(cles_6_lettres, reverse = False)
+
+#Extraire les paires (clefs, valeurs) selon une condition
+paires_sup_30 = [(cle, valeur) for cle, valeur in depart.items() if valeur > 30]
+E = [(cle, valeur) for cle, valeur in depart.items() if len(cle) <= 5]
+
+#Trier par ordre alphabétique des clés
+E_alpha = sorted(E, key=lambda x: x[0], reverse = False)
+
+#Trier par ordre décroissant des valeurs
+E_valeurs = sorted(E, key=lambda x: x[1], reverse=True)
+
+
+
+
+
+
+
+
+
+
