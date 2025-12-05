deb61b8beae9bc54ac6578bfdf926e0b3de4d28f
Marko <sinadinovic.marko.professionnel@gmail.com>
2025-12-05 15:18:18 +0100

    Update Ordre_erreur.py

--- Codes/Integration/Ordre_erreur.py
+++ Codes/Integration/Ordre_erreur.py
@@ -1,36 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# --- Paramètres de l'intégration ---
-a = 0.0                 # borne inférieure
-b = 2 * np.pi           # borne supérieure
+#Notre intégrale
+a = 0.0                 #borne inférieure
+b = 2 * np.pi           #borne supérieure
 
-# Vecteur contenant différents nombres d'intervalles pour affiner la discrétisation
+#Vecteur contenant différents nombres d'intervalles pour affiner la discrétisation
 M_values = np.array([50, 200, 400, 800])
 taille_M = M_values.size
 
-# Calcul du pas de discrétisation associé à chaque M:
+#Calcul du pas de discrétisation associé à chaque M:
 h_values = (b - a) / M_values
 
-# Valeur exacte de l'intégrale (ici donnée par l'expression issue du TP)
+#Valeur exacte de l'intégrale (ici donnée par l'expression issue du TP)
 ex = -((10 * np.pi - 3 + 3 * np.exp(2 * np.pi)) / (25 * np.exp(2 * np.pi))) * np.ones(l)
 
-# --- Méthode : Rectangles à gauche ---
-Grg = np.zeros(taille_M)  # approximations calculées
+#Méthode : Rectangle à gauche
+Grg = np.zeros(taille_M)  #approximations calculées
 for i in range(taille_M):
     Grg[i] = fonctions.intnum(a, b, M_values[i], fonctions.f2, 1)
 # Calcul de l'erreur : différence entre la valeur approchée et la valeur exacte
 erg = Grg - ex
 
-# Tracé en échelle logarithmique
+#Tracé en échelle logarithmique
 plt.figure(figsize=(10, 6))
 plt.plot(np.log(h_values), np.log(np.abs(erg)), 'o-', label="Rectangles à gauche")
 
-# Calcul des ordres numériques (pentes locales sur la courbe log-log)
+#Calcul des ordres numériques (pentes locales sur la courbe log-log)
 orders_rect_left = (np.log(np.abs(erg[1:])) - np.log(np.abs(erg[:-1]))) / (np.log(h_values[1:]) - np.log(h_values[:-1]))
 print("Ordres numériques rectangles à gauche :", orders_rect_left)
 
-# --- Méthode : Rectangles à droite ---
+#Méthode : Rectangle à droite
 Grd = np.zeros(taille_M)
 for i in range(taille_M):
     Grd[i] = fonctions.intnum(a, b, M_values[i], fonctions.f2, 2)
@@ -40,7 +40,7 @@ plt.plot(np.log(h_values), np.log(np.abs(erd)), 's-', label="Rectangles à droit
 orders_rect_right = (np.log(np.abs(erd[1:])) - np.log(np.abs(erd[:-1]))) / (np.log(h_values[1:]) - np.log(h_values[:-1]))
 print("Ordres numériques rectangles à droite :", orders_rect_right)
 
-# --- Méthode : Point milieu ---
+#Méthode : Point milieu
 Gpm = np.zeros(taille_M)
 for i in range(taille_M):
     Gpm[i] = fonctions.intnum(a, b, M_values[i], fonctions.f2, 3)
@@ -50,7 +50,7 @@ plt.plot(np.log(h_values), np.log(np.abs(epm)), '^-', label="Point milieu")
 orders_midpoint = (np.log(np.abs(epm[1:])) - np.log(np.abs(epm[:-1]))) / (np.log(h_values[1:]) - np.log(h_values[:-1]))
 print("Ordres numériques point milieu :", orders_midpoint)
 
-# --- Méthode : Trapèzes ---
+#Méthode : Trapèze
 Gt = np.zeros(taille_M)
 for i in range(taille_M):
     Gt[i] = fonctions.intnum(a, b, M_values[i], fonctions.f2, 4)
@@ -60,7 +60,7 @@ plt.plot(np.log(h_values), np.log(np.abs(et)), 'd-', label="Trapèzes")
 orders_trapezoidal = (np.log(np.abs(et[1:])) - np.log(np.abs(et[:-1]))) / (np.log(h_values[1:]) - np.log(h_values[:-1]))
 print("Ordres numériques trapèzes :", orders_trapezoidal)
 
-# --- Méthode : Simpson ---
+#Méthode : Simpson
 Gs = np.zeros(taille_M)
 for i in range(taille_M):
     Gs[i] = fonctions.intnum(a, b, M_values[i], fonctions.f2, 5)
